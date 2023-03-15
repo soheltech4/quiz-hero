52c11b8c1f27f57e2127fc0edc5772f929dd9ebf
sohelranagtc <sohelranagtc@gmail.com>
2023-03-16 00:46:14 +0600

    results error solved

--- index.js
+++ index.js
@@ -59,9 +59,7 @@ const displayQuiz = (data) => {
     quizContainer.innerHTML = "";
     return;
   }
-console.log(quizContainer)
   data.forEach((quiz, i) => {
-    console.log(quiz, i)
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
@@ -111,7 +109,7 @@ document.querySelector("#submit").addEventListener("click", function() {
   }
 
   // data setting on local storage and getting data from local storage
-  let storage = JSON.parse(localStorage.getItem("result"));
+  let storage = JSON.parse(localStorage.getItem("results"));
   if (storage) {
     localStorage.setItem(
       "results",
