840e9ea48cac35479a71d94d56bc808db1c2bbf5
sohelranagtc <sohelranagtc@gmail.com>
2023-03-16 00:16:13 +0600

    quizContainer Error Problem Solved

--- index.js
+++ index.js
@@ -55,19 +55,19 @@ const loadQuiz = async () => {
 
 // Displaying quiz on quiz page
 const displayQuiz = (data) => {
-  console.log(data)
   if (!data) {
     quizContainer.innerHTML = "";
     return;
   }
-
+console.log(quizContainer)
   data.forEach((quiz, i) => {
+    console.log(quiz, i)
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
       ${i + 1}
     </div>
-    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
+    <p class="text-gray-800 text-sm">${quiz.question}</p>
   </div>
   <div class="grid grid-cols-2 gap-4 mt-5">
     ${displayQuizOptions(quiz.options, i)}
